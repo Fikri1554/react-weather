de853f533c8e467e76267f57514c3a4427dbb99d
MuhammadFikri <udinsaprudin1456@gmail.com>
2024-12-16 16:09:27 +0700

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       setCity(cityName);
 
       const dailyForecast = forecastdata.list.filter(
-        (_item, index) => index % 8 === 0
+        (item, index) => index % 8 === 0
       );
       setForecast(dailyForecast);
     } catch (error) {
@@ -90,29 +90,29 @@ function App() {
   </>
 )}
 
-{forecast.length > 0 && (
-  <>
-    <div className="forecast">
-      <h2 className="forecast-header">5-Day Forecast</h2>
-      <div className="forecast-days">
-        {forecast.map((day, index) => (
-          <div key={index} className="forecast-day">
-            <p>
-              {new Date(day.dt * 1000).toLocaleDateString("en-US", {
-                weekday: "short",
-              })}
-            </p>
-            <img
-              src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-              alt={day.weather[0].description}
-            />
-            <p>{Math.round(day.main.temp)}°F</p>
-          </div>
-        ))}
+  {forecast.length > 0 && (
+    <>
+      <div className="forecast">
+        <h2 className="forecast-header">5-Day Forecast</h2>
+        <div className="forecast-days">
+          {forecast.map((day, index) => (
+            <div key={index} className="forecast-day">
+              <p>
+                {new Date(day.dt * 1000).toLocaleDateString("en-US", {
+                  weekday: "short",
+                })}
+              </p>
+              <img
+                src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                alt={day.weather[0].description}
+              />
+              <p>{Math.round(day.main.temp)}°F</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-)}
+    </>
+  )}
     </div>
   );
 }
