2b1d16fb69d3d24ec4b0549a2f443169fe85addc
MuhammadFikri <udinsaprudin1456@gmail.com>
2024-12-16 16:10:06 +0700

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -70,49 +70,49 @@ function App() {
       </form>
       {error && <p className="error">{error}</p>}
 
-{weatherData && weatherData.main && weatherData.weather && (
-  <>
-    <div className="header">
-      <h1 className="city">{weatherData.name}</h1>
-      <p className="temperature">{weatherData.main.temp}°F</p>
-      <p className="condition">{weatherData.weather[0].main}</p>
-    </div>
-    <div className="weather-details">
-      <div >
-        <p >Humidity</p>
-        <p style={{fontWeight:"bold"}}>{Math.round(weatherData.main.humidity)}%</p>
-      </div>
-      <div>
-        <p>Wind Speed</p>
-        <p style={{fontWeight:"bold"}}>{Math.round(weatherData.wind.speed)} mph</p>
-      </div>
-    </div>
-  </>
-)}
-
-  {forecast.length > 0 && (
-    <>
-      <div className="forecast">
-        <h2 className="forecast-header">5-Day Forecast</h2>
-        <div className="forecast-days">
-          {forecast.map((day, index) => (
-            <div key={index} className="forecast-day">
-              <p>
-                {new Date(day.dt * 1000).toLocaleDateString("en-US", {
-                  weekday: "short",
-                })}
-              </p>
-              <img
-                src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                alt={day.weather[0].description}
-              />
-              <p>{Math.round(day.main.temp)}°F</p>
-            </div>
-          ))}
+    {weatherData && weatherData.main && weatherData.weather && (
+      <>
+        <div className="header">
+          <h1 className="city">{weatherData.name}</h1>
+          <p className="temperature">{weatherData.main.temp}°F</p>
+          <p className="condition">{weatherData.weather[0].main}</p>
         </div>
-      </div>
-    </>
-  )}
+        <div className="weather-details">
+          <div >
+            <p >Humidity</p>
+            <p style={{fontWeight:"bold"}}>{Math.round(weatherData.main.humidity)}%</p>
+          </div>
+          <div>
+            <p>Wind Speed</p>
+            <p style={{fontWeight:"bold"}}>{Math.round(weatherData.wind.speed)} mph</p>
+          </div>
+        </div>
+      </>
+    )}
+
+    {forecast.length > 0 && (
+      <>
+        <div className="forecast">
+          <h2 className="forecast-header">5-Day Forecast</h2>
+          <div className="forecast-days">
+            {forecast.map((day, index) => (
+              <div key={index} className="forecast-day">
+                <p>
+                  {new Date(day.dt * 1000).toLocaleDateString("en-US", {
+                    weekday: "short",
+                  })}
+                </p>
+                <img
+                  src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                  alt={day.weather[0].description}
+                />
+                <p>{Math.round(day.main.temp)}°F</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    )}
     </div>
   );
 }
